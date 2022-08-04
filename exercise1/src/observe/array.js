7ed4904264a140ab30b18249210349d32796f662
Scorpio <18556134754@163.com>
2022-08-04 15:41:49 +0800

    some update

---
+++ exercise1/src/observe/array.js
@@ -0,0 +1,37 @@
+/* 重写数组的部分方法 */
+
+// 暂存数组的原型对象
+const oldArrayProto = Array.prototype
+
+// 生成新的数组原型对象，在新的数组原型对象上重写数组部分方法，同时保留数组的其它方法
+export const newArrayProto = Object.create(oldArrayProto)
+
+// 需要重写的 7 个变异方法列表
+const methods = ['push', 'pop', 'shift', 'unshift', 'reverse', 'sort', 'splice']
+
+methods.forEach(method => {
+    newArrayProto[method] = function (...args) {
+        const result = oldArrayProto[method].call(this, ...args)
+
+        const ob = this.__ob__
+        let inserted
+        switch (method) {
+            case 'push':
+            case 'unshift':
+                inserted = args
+                break
+            case 'splice':
+                inserted = args.slice(2)
+                break
+            default:
+                break
+        }
+        if (inserted) {
+            // 对通过 push unshift splice 方法新增的内容继续观测
+            ob.observeArray(inserted)
+        }
+
+        // 返回执行结果
+        return result
+    }
+})
--- exercise1/src/observe/index.js
+++ exercise1/src/observe/index.js
@@ -1,8 +1,12 @@
 /* 响应式模块 */
+import {newArrayProto} from './array'
 
 export function observe(data) {
     if (typeof data !== 'object' || data === null) {
         return
+    }
+    if (data.__ob__ instanceof Observer) {
+        return data.__ob__
     }
     /* 预备劫持对象 */
     return new Observer(data)
@@ -10,9 +14,17 @@ export function observe(data) {
 
 class Observer {
     constructor(data) {
+        // 防止死循环
+        // data.__ob__ = this
+        Object.defineProperty(data, '__ob__', {
+            value: this,
+            enumerable: false
+        })
+
         if (Array.isArray(data)) {
             /* 重写数组的 7个变异方法，这些方法是可以修改数组本身的，但是要保留原有的一些方法 */
-            // TODO
+            // 这里只是简化写法，Vue 源码中对这里进行了特殊处理，防止有的浏览器不支持 __proto__ 属性写法
+            data.__proto__ = newArrayProto
             /* 在这里还要对数组中的引用类型进行 observe */
             this.observeArray(data)
         } else {
@@ -24,15 +36,11 @@ class Observer {
     walk(data) {
         /* 遍历对象所有属性重新定义属性，是 Vue2 的性能瓶颈之一 */
         /* 且只能遍历初始定义好的属性，也是 Vue2 新增 $set $delete api 的原因 */
-        Object.keys(data).forEach(key => {
-            defineReactive(data, key, data[key])
-        })
+        Object.keys(data).forEach(key => defineReactive(data, key, data[key]))
     }
 
     observeArray(data) {
-        data.forEach(item => {
-            observe(item)
-        })
+        data.forEach(item => observe(item))
     }
 }
 
@@ -47,6 +55,8 @@ export function defineReactive(target, key, value) {
         },
         set(v) {
             if (v === value) return
+            // 赋新值时，如果是引用类型，继续劫持
+            observe(v)
             value = v
         }
     })
