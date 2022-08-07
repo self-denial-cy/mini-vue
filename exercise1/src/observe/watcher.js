b1ee2ed76ba8c7168557005fa84ae364dcedf9f5
白夜 <18556134754@163.com>
2022-08-07 22:13:32 +0800

    some update

--- exercise1/src/observe/watcher.js
+++ exercise1/src/observe/watcher.js
@@ -34,8 +34,59 @@ class Watcher {
     }
 
     update() {
+        queueWatcher(this)
+    }
+
+    run() {
         this.get()
     }
+}
+
+let queue = []
+let has = {}
+let pending = false // 防抖
+
+function queueWatcher(watcher) {
+    const id = watcher.id
+    // 针对同一个 Watcher 的连续触发，只记录第一次
+    if (!has[id]) {
+        queue.push(watcher)
+        has[id] = true
+        // 针对多个 Watcher 被触发，设置一个异步刷新任务，最终一次性刷新
+        if (!pending) {
+            setTimeout(flushSchedulerQueue, 0)
+            pending = true
+        }
+    }
+}
+
+function flushSchedulerQueue() {
+    const flushQueue = queue.slice(0)
+    queue = []
+    has = {}
+    pending = false
+    flushQueue.forEach(q => q.run())
+}
+
+let callbacks = []
+let waiting = false
+
+// nextTick 将传入的回调函数缓存成一个异步执行队列，与 Watcher 的异步刷新队列拉到了同一维度
+// Vue 中并没有直接采用某个 api 而是采用了优雅降级的方式
+// Promise（不兼容 ie） MutationObserver（h5 api） setImmediate（ie 独有） setTimeout
+export function nextTick(cb) {
+    callbacks.push(cb)
+    if (!waiting) {
+        setTimeout(flushCallbacks, 0)
+        waiting = true
+    }
+}
+
+function flushCallbacks() {
+    const cbs = callbacks.slice(0)
+    callbacks = []
+    waiting = false
+    cbs.forEach(cb => cb())
 }
 
 // 一个组件对应着多个属性 一个属性可以在多个组件中使用 组件和属性的关系是多对多
