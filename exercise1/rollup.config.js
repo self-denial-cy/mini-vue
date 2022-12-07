2d412f6f5d178b7dbf3ebc4af17d1f86c3324039
白夜 <18556134754@163.com>
2022-12-08 00:49:05 +0800

    chore: 优化 rollup 构建

--- exercise1/rollup.config.js
+++ exercise1/rollup.config.js
@@ -1,14 +1,83 @@
-import babel from 'rollup-plugin-babel'
+const path = require('path');
+const resolve = require('@rollup/plugin-node-resolve');
+const commonjs = require('@rollup/plugin-commonjs');
+const babel = require('@rollup/plugin-babel');
+const {terser} = require('rollup-plugin-terser');
+const clear = require('rollup-plugin-clear');
+const progress = require('rollup-plugin-progress');
+const sizes = require('rollup-plugin-sizes');
+const filesize = require('rollup-plugin-filesize');
+const pkg = require('./package.json');
 
-export default {
-    input: './src/index.js',
-    output: {
-        file: './dist/vue.js',
-        name: 'Vue',
-        format: 'umd',
-        sourcemap: true
+const isProd = process.env.NODE_ENV === 'production';
+
+module.exports = [
+    {
+        input: path.join(__dirname, './src/index.js'),
+        output: {
+            name: 'Vue',
+            file: path.join(__dirname, pkg.browser),
+            format: 'iife',
+            sourcemap: !isProd
+        },
+        plugins: [
+            resolve(), // so Rollup can find module from node_modules
+            commonjs(), // so Rollup can convert module from node_modules to an ES module
+            babel({
+                exclude: ['node_modules/**'],
+                babelHelpers: 'bundled',
+                presets: [['@babel/preset-env', {
+                    useBuiltIns: false,
+                    modules: false
+                }]],
+                plugins: [['@babel/plugin-transform-runtime', {
+                    corejs: 3,
+                    helpers: false,
+                    regenerator: false, // helpers 为 false 时，regenerator 参数无效
+                    version: '^7.8.4'
+                }]]
+            }),
+            isProd ? terser() : null,
+            clear({
+                targets: ['dist']
+            }),
+            progress(),
+            sizes(),
+            filesize()
+        ]
     },
-    plugins: [babel({
-        exclude: 'node_modules/**'
-    })]
-}
+    {
+        input: path.join(__dirname, './src/index.js'),
+        output: {
+            file: path.join(__dirname, pkg.module),
+            format: 'es',
+            sourcemap: !isProd
+        },
+        plugins: [
+            resolve(),
+            commonjs(),
+            babel({
+                exclude: ['node_modules/**'],
+                babelHelpers: 'runtime',
+                presets: [['@babel/preset-env', {
+                    useBuiltIns: false,
+                    modules: false
+                }]],
+                plugins: [['@babel/plugin-transform-runtime', {
+                    corejs: 3,
+                    helpers: true,
+                    regenerator: true,
+                    version: '^7.8.4'
+                }]]
+            }),
+            isProd ? terser() : null,
+            clear({
+                targets: ['dist']
+            }),
+            progress(),
+            sizes(),
+            filesize()
+        ],
+        external: id => id.includes('@babel/runtime-corejs3')
+    }
+];
